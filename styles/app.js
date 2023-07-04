4bd23b154f8d31d4583209590a64ca0fa2ba9645
Angel Alfaro <angelalfarox@gmail.com>
2023-07-03 20:36:18 -0400

    deploy

--- styles/app.js
+++ styles/app.js
@@ -30,13 +30,13 @@ var commands = {
     whoami: function() {
         this.echo("I am passionate about web development as well as low level programming like working on Kernels for Operating Systems. \n" +
             "Previously I worked at " +  hotpink("Deloitte Consulting") + " as an " + green("IT associate") + ", before that I was a " + green("Software Engineering Intern") + 
-            " at Both: " + hotpink("Dell Technlogies") + " & " + hotpink("LaunchGood.") + " I am now a" + "full-time" + green(" Software Engineer\n") + " @ " + hotpink("NextEra Energy.") +
+            " at Both: " + hotpink("Dell Technlogies") + " & " + hotpink("LaunchGood.") + " I am now a" + "full-time" + green(" Software Engineer \n") + " @ " + hotpink("NextEra Energy.") +
             " I graduated with my " + green("Bachelor's") + " in " + green("Computer Science") + " from " + blue("Florida International"
             + " University") + " with " + gold("Cum Laude Honors.\n\n") +
             green("Languages:") + " Python, Java, javascript & C \n\n");
     },
     projects: function() {
-        this.echo("I'm currently working on building my own compilers for C and Java")
+        this.echo("I'm currently working on building my own compilers for C and Java \n" + "As well as working on something special at " + green("EverythingButCode.io"));
     },
     contact: function() {
         this.echo("Email: AngelAlfarox at gmail dot com\n" +
